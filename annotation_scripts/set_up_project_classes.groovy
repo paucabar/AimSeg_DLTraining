4eb9f7d7c58536034aeb37ea201b8c6b349108da
Pau Carrillo-Barbera <pau.carrillob@gmail.com>
2023-02-28 18:24:03 +0000

    script to set up project classes in QuPath

---
+++ annotation_scripts/set_up_project_classes.groovy
@@ -0,0 +1,12 @@
+// Platform.runLater to call on the correct (UI) thread
+Platform.runLater {
+    // Be really careful here!
+    // Need to include null for unclassified objects, and make sure all names are unique
+    getQuPath().getAvailablePathClasses().setAll(
+        getPathClass(null), // Important to keep this!
+        getPathClass('AxonNMy', makeRGB(255, 0, 255)), 
+        getPathClass('Fibre', makeRGB(0, 0, 255)),
+        getPathClass('InMy', makeRGB(255, 0, 0)),
+        getPathClass('AxonMy', makeRGB(255, 255, 0))
+    )
+}
